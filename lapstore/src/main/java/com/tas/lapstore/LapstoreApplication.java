5185694fa1817743d1d3d8c1b079d059d441ceca
unknown <wijemobapp@gmail.com>
2025-08-04 17:59:01 +0530

    project 01

---
+++ lapstore/src/main/java/com/tas/lapstore/LapstoreApplication.java
@@ -0,0 +1,13 @@
+package com.tas.lapstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapstoreApplication.class, args);
+	}
+
+}
---
+++ lapstore/src/test/java/com/tas/lapstore/LapstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tas.lapstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
